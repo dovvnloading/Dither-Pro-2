e33f4415ca733b0dcf6442e2eaf97450b3fc1003
Matthew Wesney <157447210+dovvnloading@users.noreply.github.com>
2025-12-29 16:41:40 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
---- START OF FILE vite.config.ts ---
-
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // IMPORTANT: This must match your repo name exactly
   base: '/Dither-Pro-2/',
   server: {
     port: 3000,
